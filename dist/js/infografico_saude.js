76e5bebbc6ab0c3d0a5fc597a85f9b04c99b58b3
Clístenes Onassis <clistenes808@gmail.com>
2019-05-21 03:12:10 -0300

    atualização da plataforma
    modificações em todas as areas da plataforma.

---
+++ dist/js/infografico_saude.js
@@ -0,0 +1,280 @@
+$(function(){
+	$('#tipoGrafico').change(function(){
+
+		var option = $('#selectOption').find(":selected").attr('value');
+		var tipo_de_grafico = $('#tipoGrafico').find(":selected").attr('value');
+
+		if (tipo_de_grafico == "forma" && option == "1") {
+			$('#grafico').html("");
+		}
+		else if(tipo_de_grafico == "barra" && option == "1"){
+			$('#grafico').html("");
+		}
+		else if(tipo_de_grafico == "doughnut" && option == "1"){
+			$('#grafico').html("");
+		}
+		else if(tipo_de_grafico == "linhaDoTempo" && option == "1"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "1") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "1") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "2") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "2") {
+			$('#grafico').html("");
+		}
+		else if(tipo_de_grafico =='doughnut' && option == "2"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "2"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "2") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "2") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "3"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "4"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "5"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "6"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "7") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "7") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "7") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "7"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "7") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "7") {
+			$('#grafico').html("");
+		}
+
+	});
+});
+
+//T2 - TIPOS DE CRIMES - SEGURANÇA//
+$(function(){
+	//##################################//
+	//INICIANDO COM ITEM PRÉ-SELECIONADO//
+	//##################################//
+	$('#grafico').html("<div class='tableauPlaceholder' id='viz1558124840168' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ta&#47;tabelasobreEducao&#47;Painel1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='tabelasobreEducao&#47;Painel1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ta&#47;tabelasobreEducao&#47;Painel1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1558124840168');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>");	
+	//###################################//
+	//QUANDO O USUÁRIO SELECIONAR UM ITEM//
+	//###################################//
+	$('#selectOption').change(function() {
+
+		var option = $('#selectOption').find(":selected").attr('value');
+		var tipo_de_grafico = $('#tipoGrafico').find(":selected").attr('value');
+
+		if (tipo_de_grafico == "forma" && option == "1") {
+			$('#grafico').html("");
+		}
+		else if(tipo_de_grafico == "barra" && option == "1"){
+			$('#grafico').html("");
+		}
+		else if(tipo_de_grafico == "doughnut" && option == "1"){
+			$('#grafico').html("");
+		}
+		else if(tipo_de_grafico == "linhaDoTempo" && option == "1"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "1") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "1") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "2") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "2") {
+			$('#grafico').html("");
+		}
+		else if(tipo_de_grafico =='doughnut' && option == "2"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "2"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "2") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "2") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "3"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "3") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "4"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "4") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "5"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "5") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "6"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "6") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'forma' && option == "7") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'barra' && option == "7") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'doughnut' && option == "7") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'linhaDoTempo' && option == "7"){
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2016' && option == "7") {
+			$('#grafico').html("");
+		}
+		else if (tipo_de_grafico == 'mapa2017' && option == "7") {
+			$('#grafico').html("");
+		}
+
+	});
+});
+//T2 - TIPOS DE CRIMES - SEGURANÇA//
